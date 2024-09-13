d945f82be409a8411a3311b43db797c6d5a7004e
Andrewnathan7305 <andrewnathan0704@gmail.com>
2024-09-14 01:37:12 +0530

    token

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,33 +1,73 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Add.css'
 import { assets } from '../../assets/assets'
+import { useState } from 'react'
+import axios from "axios"
+import { toast } from 'react-toastify'
 
-const Add = () => {
+const Add = ({url}) => {
+    const [image,setImage] = useState(false);
+    const [data,setData] = useState({
+        name:"",
+        description:"",
+        price:"",
+        category:"Salad"
+    })
+    const onChangeHandler = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data=>({...data,[name]:value}))
+    }
+    
+    const onSubmitHandler = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append("name",data.name)
+        formData.append("description",data.description)
+        formData.append("price",Number(data.price))
+        formData.append("category",data.category)
+        formData.append("image",image)
+        const response = await axios.post(`${url}/api/food/add`,formData)
+        if(response.data.success){
+            setData({
+                name:"",
+                description:"",
+                price:"",
+                category:"Salad"
+            })
+            setImage(false)
+            toast.success(response.data.message)
+        }
+        else{
+            toast.error(response.data.message)
+        }
+    }
+    
     return (
         <div>
             <div className="add">
-                <form className='flex-col'>
+                <form className='flex-col' onSubmit={onSubmitHandler}>
                     <div className="add-img-upload flex-col">
                         <p>upload Image</p>
                         <label htmlFor="image">
-                            <img src={assets.upload_area} alt="" />
+                            <img src={image?URL.createObjectURL(image):assets.upload_area} alt="" />
                         </label>
-                        <input type="file" id="image" hidden required />
+                        <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="image" hidden required />
 
                     </div>
                     <div className="add-product-name flex-col">
                         <p>product name</p>
-                        <input type="text" name="name" placeholder='type here' />
+                        <input onChange={onChangeHandler} value={data.name} type="text" name="name" placeholder='type here' />
 
                     </div>
                     <div className="add-product-description flexcol">
                         <p>product description</p>
-                        <textarea name="description" row="g" placeholder='write content here'></textarea>
+                        <textarea onChange={onChangeHandler} value={data.description} name="description" row="g" placeholder='write content here'></textarea>
                     </div>
                     <div className="add-category-price flex-col">
                         <div className="add-category">
                             <p>product category</p>
-                            <select name="category" id="">
+                            <select onChange={onChangeHandler} value={data.name} name="category" id="">
                                 <option value="Salad">Salad</option>
                                 <option value="Rolls">Rolls</option>
                                 <option value="Deserts">Deserts</option>
@@ -40,7 +80,7 @@ const Add = () => {
                         </div>
                         <div className="add-price flex-col">
                             <p>Product price</p>
-                            <input type="Number" name='price' placeholder='$20' />
+                            <input onChange={onChangeHandler} value={data.price} type="Number" name='price' placeholder='$20' />
                         </div>
                     </div>
                     <button type='submit' className='add-btn'>ADD</button>
